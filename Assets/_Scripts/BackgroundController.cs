2cf12101fed0426f8b7849286aefe9428a6eef12
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-20 14:29:19 -0400

    changed background to move horizontally. updated its scale and speed

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// BackgroundController.cs
+/// Michael Denkovski 101222288
+/// GAME 2014 Mobile Game Dev
+/// Last Modified Oct 20
+/// - changed variable names to reflect horizontal movement orientation in landscape
+/// </summary>
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -14,20 +21,29 @@ public class BackgroundController : MonoBehaviour
         _CheckBounds();
     }
 
+    /// <summary>
+    /// reset the position based on the horizontal boundary
+    /// </summary>
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
+    /// <summary>
+    /// move the background to the left(-x axis) based on its horizontal speed
+    /// </summary>
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
+    /// <summary>
+    /// check the horizontal bounds if the background is past
+    /// </summary>
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
