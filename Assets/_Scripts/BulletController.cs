f73c7bb98a6a1fc9e427cfc779ac0671d81d4648
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-20 14:17:43 -0400

    CHnaged bullets to move horizontally left to right as well as their orientation

--- Assets/_Scripts/BulletController.cs
+++ Assets/_Scripts/BulletController.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// BulletController.cs
+/// Michael Denkovski 101222288
+/// GAME 2014 Mobile Game Dev
+/// Last Modified Oct 20
+/// - changed variable names to reflect horizontal movement orientation in landscape
+/// </summary>
 public class BulletController : MonoBehaviour, IApplyDamage
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
     public BulletManager bulletManager;
     public int damage;
     
@@ -22,14 +30,20 @@ public class BulletController : MonoBehaviour, IApplyDamage
         _CheckBounds();
     }
 
+    /// <summary>
+    /// move the bullet along the x axis based on its horizontal speed
+    /// </summary>
     private void _Move()
     {
-        transform.position += new Vector3(0.0f, verticalSpeed, 0.0f) * Time.deltaTime;
+        transform.position += new Vector3(horizontalSpeed, 0.0f, 0.0f) * Time.deltaTime;
     }
 
+    /// <summary>
+    /// check the bullet if it is past the horixontal boundary
+    /// </summary>
     private void _CheckBounds()
     {
-        if (transform.position.y > verticalBoundary)
+        if (transform.position.x > horizontalBoundary)
         {
             bulletManager.ReturnBullet(gameObject);
         }
