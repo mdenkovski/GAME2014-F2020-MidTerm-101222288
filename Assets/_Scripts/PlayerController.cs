fef4af487009831037fc7801005a9b7d6da583cf
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-20 18:10:59 -0400

    change player position/orientation based on layout

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     [Header("Boundary Check")]
     public float verticalBoundary;
+    public float horizontalBoundary;
 
     /// <summary>
     /// the speed at which the player can move
@@ -29,6 +30,8 @@ public class PlayerController : MonoBehaviour
     public float verticalSpeed;
     public float maxSpeed;
     public float verticalTValue;
+    public float horizontalSpeed;
+    public float horizontalTValue;
 
     [Header("Bullet Firing")]
     public float fireDelay;
@@ -36,23 +39,85 @@ public class PlayerController : MonoBehaviour
     // Private variables
     private Rigidbody2D m_rigidBody;
     private Vector3 m_touchesEnded;
+    private bool m_bOrientationChanged;
+    ScreenOrientation m_screenOrientation;
 
     // Start is called before the first frame update
     void Start()
     {
         m_touchesEnded = new Vector3();
         m_rigidBody = GetComponent<Rigidbody2D>();
+        m_screenOrientation = Screen.orientation;
+        m_bOrientationChanged = false;
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            _SetLandscapePosition();
+        }
+        else
+        {
+            _SetPortraitPosition();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _Move();
-        _CheckBounds();
-        _FireBullet();
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight)
+        { 
+            //update our internal orientation to landcape
+            if (m_screenOrientation != ScreenOrientation.LandscapeLeft && m_screenOrientation != ScreenOrientation.LandscapeRight)
+            {
+                m_screenOrientation = Screen.orientation;
+                m_bOrientationChanged = true;
+                Debug.Log("orientation changed");
+            }
+            if (m_bOrientationChanged)
+            {
+                _SetLandscapePosition();
+            }
+            _Move();
+            _CheckBounds();
+            _FireBullet();
+        }
+        if(Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+        {
+            //update our internal orientation to portrait
+            if (m_screenOrientation != ScreenOrientation.Portrait && m_screenOrientation  != ScreenOrientation.PortraitUpsideDown)
+            {
+                m_screenOrientation = Screen.orientation;
+                m_bOrientationChanged = true;
+                Debug.Log("orientation changed");
+            }
+            if (m_bOrientationChanged)
+            {
+                _SetPortraitPosition();
+            }
+            _MoveHorizontal();
+            _CheckBoundsHorizontal();
+            _FireBullet();
+        }
+        Debug.Log(Screen.orientation);
+
     }
 
-     private void _FireBullet()
+    /// <summary>
+    /// move the player to the default landscape position
+    /// </summary>
+    private void _SetLandscapePosition()
+    {
+        //transform.position = new Vector2(-7.44f, 0.0f);
+        //transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+        transform.SetPositionAndRotation(new Vector2(-7.44f, 0.0f), Quaternion.Euler(0.0f, 0.0f, -90.0f));
+    }
+
+    private void _SetPortraitPosition()
+    {
+        //transform.position = new Vector2(0.0f, -4.0f); 
+        Quaternion.Euler(0.0f, 0.0f, 90.0f);
+        transform.SetPositionAndRotation(new Vector2(0.0f, -4.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f));
+    }
+
+    private void _FireBullet()
     {
         // delay bullet firing 
         if(Time.frameCount % 60 == 0 && bulletManager.HasBullets())
@@ -117,7 +182,62 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// check the vertical bounds of the ship to make sure it is within
+    /// move the player along the x axis left or right
+    /// </summary>
+    private void _MoveHorizontal()
+    {
+        float direction = 0.0f;
+
+        // touch input support
+        foreach (var touch in Input.touches)
+        {
+            var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (worldTouch.x > transform.position.x)
+            {
+                // direction is positive
+                direction = 1.0f;
+            }
+
+            if (worldTouch.y < transform.position.y)
+            {
+                // direction is negative
+                direction = -1.0f;
+            }
+
+            m_touchesEnded = worldTouch;
+
+        }
+        //use the horizontal axis for the movememnt
+        // keyboard support
+        if (Input.GetAxis("Horizontal") >= 0.1f)
+        {
+            // direction is positive
+            direction = 1.0f;
+        }
+        //use the horizontal axis for the movememnt
+        if (Input.GetAxis("Horizontal") <= -0.1f)
+        {
+            // direction is negative
+            direction = -1.0f;
+        }
+
+        if (m_touchesEnded.x != 0.0f)
+        {
+            //move the player along the x axis
+            transform.position = new Vector2(Mathf.Lerp(transform.position.x, m_touchesEnded.x, horizontalTValue), transform.position.y);
+        }
+        else
+        {
+            //set y to 0 and x value based on direction and speed
+            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+            m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
+            m_rigidBody.velocity *= 0.99f;
+        }
+    }
+
+    /// <summary>
+    /// check the vertical bounds of the player to make sure it is within
     /// </summary>
     private void _CheckBounds()
     {
@@ -133,6 +253,27 @@ public class PlayerController : MonoBehaviour
         {
             //move the player in bounds based on vertical boundary
             transform.position = new Vector3(transform.position.x, -verticalBoundary, 0.0f);
+        }
+
+    }
+    
+    /// <summary>
+    /// check the horizontal bounds of the player to make sure it is within
+    /// </summary>
+    private void _CheckBoundsHorizontal()
+    {
+        // check top bounds
+        if (transform.position.x >= horizontalBoundary)
+        {
+            //move the player in bounds based on horizontal boundary
+            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+        }
+
+        // check bottom bounds
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            //move the player in bounds based on horizontal boundary
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
         }
 
     }
